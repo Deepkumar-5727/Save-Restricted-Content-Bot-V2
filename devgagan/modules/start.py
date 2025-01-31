241c672a64728770dc9a49e6e213864db944a4ff
Deepkumar-5727 <deep_kumar@myyahoo.com>
2025-01-31 23:13:12 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -166,7 +166,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/pushparaj243")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
@@ -185,7 +185,7 @@ async def plan(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/pushparaj243")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
